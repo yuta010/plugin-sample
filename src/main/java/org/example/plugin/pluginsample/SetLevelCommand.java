bf2dd3fa1c5e1ace7b9044c2951042c9a3b0b4bd
yuta <kuromame.guri@gmail.com>
2023-08-22 15:40:09 +0900

    setlevelコマンドを実装

---
+++ src/main/java/org/example/plugin/pluginsample/SetLevelCommand.java
@@ -0,0 +1,19 @@
+package org.example.plugin.pluginsample;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetLevelCommand implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if(sender instanceof Player player){
+      if(args.length == 1){
+       player.setLevel(Integer.parseInt(args[0]));
+      }
+    }
+    return false;
+  }
+}
